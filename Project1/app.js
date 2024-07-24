56c374b6b60ace811007a389957d551f32bf268d
Woraphob <Woraphob2349@gmail.com>
2024-07-24 15:06:20 +0700

    Initial commit

---
+++ Project1/app.js
@@ -0,0 +1,11 @@
+// let number = prompt("Enter number: ")
+// console.log(number)
+// rand = Math.floor(Math.random() * 100)
+// console.log(rand)
+// document.getElementById("result").innerHTML = rand
+// document.getElementById("number").innerHTML = number
+console.log(1=="1")
+console.log(3==4)
+console.log("5" === 4)
+console.log("phob" !== "phob")
+console.log(10>5)
---
+++ Project2/app.js
@@ -0,0 +1,4 @@
+let age = prompt("กรุณากรอกอายุของคุณ")
+if(age > 13){
+        document.getElementById("content").innerHTML = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/s9U2-SFm-Xk?si=ro8B3PyRHpabdETb\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>"
+    }

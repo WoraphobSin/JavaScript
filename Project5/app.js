ac1749d8f0294885a8767f8e450344451f63ef30
Woraphob <Woraphob2349@gmail.com>
2024-07-25 10:28:24 +0700

    Add files

--- Project5/app.js
+++ Project5/app.js
@@ -1,4 +1,22 @@
-function sayHello(){
-    alert("Hello")
+function toCelsius()
+{
+    let fahrenheit = prompt("Fahrenheit:")
+    let celsius = (parseFloat(fahrenheit) - 32) * 5 / 9
+    document.getElementById("temp").innerHTML = celsius.toFixed(2) + "  °C"
 }
-sayHello()
+
+function toFahrenheit()
+{
+    let celsius = prompt("Celsius:")
+    let fahrenheit = (celsius * 1.8) + 32
+    document.getElementById("temp").innerHTML = fahrenheit.toFixed(2) + " °F"
+}
+var value = prompt("Enter 1 for convert F to C or 2 for convert C to F")
+if(value == 1)
+{
+    toCelsius()
+}
+else if(value == 2)
+    {
+        toFahrenheit()
+    }
---
+++ Project5/app2.js
@@ -0,0 +1,10 @@
+function sayHello()
+{
+    return "Hello"
+}
+function getAge()
+{
+    return 28
+}
+alert(typeof sayHello())
+alert(typeof getAge())
